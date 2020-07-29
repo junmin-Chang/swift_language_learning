e876445ab05849132d8049b2152f1ea429e697b0
junminChang <chjm219@kookmin.ac.kr>
2020-07-29 15:50:03 +0900

    comment and tuple

---
+++ Swift_Language_learning.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//var str = "Hello, playground"
+//
+//
+//
+//
+//let value = arc4random_uniform(100)
+//print("---> \(value)")
+//
+
+
+// -- Tuple
+
+let coordinates = (4, 6)
+
+let x = coordinates.0
+let y = coordinates.1
+
+let coordinatesName = (x: 2, y: 3)
+let x2 = coordinatesName.x
+let y2 = coordinatesName.y
+
+
+let (x3, y3) = coordinatesName
+
+x3
+y3
